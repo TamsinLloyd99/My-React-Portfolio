88d8a30859ab5ddc5bf13aee9f24cef36797e2a5
Tamsin Lloyd <tamsin_lloyd@hotmail.com>
2024-03-06 22:07:54 +0000

    fixed video background to make it fit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,36 +1,39 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 import "./Contact.css";
 
 function Contact() {
+const iconSize = "3x";
+
   return (
     <>
-  <section className="contact" id="Contact">
-    <h1 className="display-5 contactTitle">Contact Me</h1>
-    <ul className="contactlist">
-      <li>
-        <a href="mailto:tamsin_lloyd@hotmail.com">
-          <i className="fas fa-envelope"></i>
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/TamsinLloyd99" target="_blank">
-          <i className="fab fa-github"></i>
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://www.linkedin.com/in/tamsin-l-423584106/"
-          target="_blank"
-        >
-          <i className="fab fa-linkedin"></i>
-        </a>
-      </li>
-    </ul>
-  </section>
-</>
-
+      <section className="contact container" id="Contact">
+        <h1 className="display-5 contactTitle">Contact Me</h1>
+        <ul className="contactlist">
+          <li>
+            <a href="mailto:tamsin_lloyd@hotmail.com">
+              <FontAwesomeIcon icon={faEnvelope} size={iconSize}/>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/TamsinLloyd99" target="_blank">
+              <FontAwesomeIcon icon={faGithub} size={iconSize} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/tamsin-l-423584106"
+              target="_blank">
+              <FontAwesomeIcon icon={faLinkedin} size={iconSize} />
+            </a>
+          </li>
+        </ul>
+      </section>
+    </>
   );
 }
 
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -8,6 +8,7 @@ function HomePage() {
     <>
       <div className="embed-responsive embed-responsive-16by9">
         <video
+          id="background-video"
           className="embed-responsive-item"
           autoPlay
           loop
@@ -17,10 +18,10 @@ function HomePage() {
           <source src={BackgroundVideo} type="video/mp4" />
         </video>
         <div className="text-overlay position-absolute top-50 start-50 translate-middle text-center text-white">
-        <h1>Front-End Web Developer</h1>
-        <br></br>
-        <h3>Crafting Digital Experiences, One Line of Code at a Time</h3>
-      </div>
+          <h1>Front-End Web Developer</h1>
+          <br></br>
+          <h3>Crafting Digital Experiences, One Line of Code at a Time</h3>
+        </div>
       </div>
     </>
   );
