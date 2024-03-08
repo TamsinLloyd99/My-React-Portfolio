fbcb2c3f5c36df02deef6409dc9aff318e2635c5
Tamsin Lloyd <tamsin_lloyd@hotmail.com>
2024-03-08 21:08:26 +0000

    made homepage media responsive

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,30 +1,24 @@
-import React from "react";
-import "./HomePage.css";
-import BackgroundVideo from "../Videos/Background.mp4";
-import ScreenShot from "../Images/Screenshot.png";
+import React from 'react';
+import './HomePage.css'; // Import your CSS file
+import BackGroundVideo from '../Videos/Background.mp4';
+import ScreenShot from '../Images/Screenshot.png';
 
-function HomePage() {
+const HomePage = () => {
   return (
-    <>
-      <div className="embed-responsive embed-responsive-16by9">
-        <video
-          id="background-video"
-          className="embed-responsive-item"
-          autoPlay
-          loop
-          muted
-          poster={ScreenShot}
-        >
-          <source src={BackgroundVideo} type="video/mp4" />
-        </video>
-        <div className="text-overlay position-absolute top-50 start-50 translate-middle text-center text-white">
-          <h1>Front-End Web Developer</h1>
-          <br></br>
-          <h3>Crafting Digital Experiences, One Line of Code at a Time</h3>
-        </div>
+    <div id="video-container">
+      <video autoPlay loop muted>
+        {/* Provide fallback image for browsers that don't support video */}
+        <source src={BackGroundVideo} type="video/mp4" />
+        {/* Fallback image */}
+        <img src={ScreenShot} alt="Fallback Image" />
+      </video>
+      <div className="text-overlay">
+        <h1>Front-End Web Developer</h1>
+        <br />
+        <h3>Crafting Digital Experiences, One Line of Code at a Time</h3>
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default HomePage;
